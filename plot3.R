d232a846e4c2d8408eae38b972a4de7eccd4802d
Phred Lender <shemp9999@gmail.com>
2014-12-06 08:56:31 -0800

    Ready for grading

--- plot3.R
+++ plot3.R
@@ -1,6 +1,5 @@
 require(data.table)
 
-
 # Read top two rows of dataset to get column classes and variable names
 top <- read.table('household_power_consumption.txt',
                   nrows=2,
@@ -11,7 +10,6 @@ top <- read.table('household_power_consumption.txt',
 classes <- sapply(top,class)
 headers <- colnames(top)
 
-
 # Read the portion of the dataset including dates to be plotted (1 MB RAM).
 #   (reading full dataset requires > 100 MB RAM)
 d <- data.table(read.table(pipe('grep ^[1-2]/2/2007 household_power_consumption.txt'), 
@@ -21,11 +19,9 @@ d <- data.table(read.table(pipe('grep ^[1-2]/2/2007 household_power_consumption.
                    header=F,
                    sep=';'))
 
-
 # combine date and time columns
 datetime <- as.POSIXlt(paste(d$Date,d$Time), 
                        format='%d/%m/%Y %H:%M:%S')
-
 
 # Create Plot 3
 png(filename='plot3.png', 
@@ -48,4 +44,3 @@ with(d, lines(datetime,
               type='l',
               col='blue'))
 dev.off()
-
